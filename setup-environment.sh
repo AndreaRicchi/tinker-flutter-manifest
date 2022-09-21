749068d08d3c2b83e885e76ca69ba84ccb1344b9
Andrea Ricchi <andrea.ricchi@amarulasolutions.com>
2022-09-21 17:38:54 +0200

    Add Flutter dependencies and packages
    Signed-off-by: Andrea Ricchi <andrea.ricchi@amarulasolutions.com>

--- setup-environment.sh
+++ setup-environment.sh
@@ -24,3 +24,16 @@ sed -i "s|MACHINE ??= \"qemux86-64\"|MACHINE ??= \"tinker-board-s\"|g" ./conf/lo
 echo '' >>./conf/local.conf
 echo 'MACHINE_EXTRA_RRECOMMENDS += "kernel-modules"' >>./conf/local.conf
 echo 'BB_NUMBER_THREADS ?= "4"' >>./conf/local.conf
+
+echo 'INIT_MANAGER = "systemd"' >>./conf/local.conf
+echo 'DISTRO_FEATURES:append = " wayland pam"' >>./conf/local.conf
+
+echo '
+IMAGE_INSTALL:append = " \
+    flutter-engine-runtimedebug \
+    flutter-pi-runtimedebug \
+    flutter-gallery-runtimedebug \
+"
+' >>./conf/local.conf
+
+echo 'TOOLCHAIN_HOST_TASK:append = " nativesdk-flutter-sdk"' >>./conf/local.conf
